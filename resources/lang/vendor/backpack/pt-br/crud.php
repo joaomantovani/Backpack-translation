4b2e1162c94d4ac81cf25ee407667dd9fd3a089a
João Pedro Mantovani <web3@marcasbr.com>
2017-02-20 11:36:45 -0300

    Add new CRUD translations text

--- resources/lang/vendor/backpack/pt-br/crud.php
+++ resources/lang/vendor/backpack/pt-br/crud.php
@@ -4,7 +4,7 @@ return [
 
     /*
     |--------------------------------------------------------------------------
-    | Dick Crud Language Lines
+    | Backpack Crud Language Lines
     |--------------------------------------------------------------------------
     |
     | The following language lines are used by the CRUD interface.
@@ -13,17 +13,18 @@ return [
     |
     */
 
+    // Forms
+    'save_action_save_and_new' => 'Salvar e adicionar um novo',
+    'save_action_save_and_edit' => 'Salvar e editar este item',
+    'save_action_save_and_back' => 'Salvar e voltar',
+    'save_action_changed_notification' => 'Default behaviour after saving has been changed.',
+    'save_action_changed_notification' => 'Comportamento padrão após salvar foi alterado.',
+
     // Create form
     'add'                 => 'Adicionar',
     'back_to_all'         => 'Voltar para todos',
     'cancel'              => 'Cancelar',
     'add_a_new'           => 'Adicionar um novo ',
-
-        // Create form - advanced options
-        'after_saving'            => 'Depois de salvar',
-        'go_to_the_table_view'    => 'Ir para todos os itens',
-        'let_me_add_another_item' => 'Me deixe adicionar outro item',
-        'edit_the_new_item'       => 'Editar esse novo item',
 
     // Edit form
     'edit'                 => 'Editar',
@@ -52,36 +53,36 @@ return [
     'details_row_loading_error' => 'Ocorreu um problema ao carregar, tente novamente.',
 
         // Confirmation messages and bubbles
-        'delete_confirm'                              => 'Você tem certeza que deseja deletar esse item?',
-        'delete_confirmation_title'                   => 'Item deletado',
-        'delete_confirmation_message'                 => 'O item foi deletado com sucesso.',
-        'delete_confirmation_not_title'               => 'NÃO deletado',
-        'delete_confirmation_not_message'             => "Houve um erro. Seu item pode não ter sido deletado.",
-        'delete_confirmation_not_deleted_title'       => 'Não deletado',
-        'delete_confirmation_not_deleted_message'     => 'Nada aconteceu, seu item está a salvo.',
+    'delete_confirm'                              => 'Você tem certeza que deseja deletar esse item?',
+    'delete_confirmation_title'                   => 'Item deletado',
+    'delete_confirmation_message'                 => 'O item foi deletado com sucesso.',
+    'delete_confirmation_not_title'               => 'NÃO deletado',
+    'delete_confirmation_not_message'             => "Houve um erro. Seu item pode não ter sido deletado.",
+    'delete_confirmation_not_deleted_title'       => 'Não deletado',
+    'delete_confirmation_not_deleted_message'     => 'Nada aconteceu, seu item está a salvo.',
 
         // DataTables translation
-        'emptyTable'     => 'Nenhum registro no momento',
-        'info'           => 'Mostrando _START_ de um total de _END_ de _TOTAL_ registros',
-        'infoEmpty'      => 'Mostrando 0 de 0 de um total de 0 registros',
-        'infoFiltered'   => '(filtrado De _MAX_ entradas totais)',
-        'infoPostFix'    => '',
-        'thousands'      => ',',
-        'lengthMenu'     => '_MENU_ itens por página',
-        'loadingRecords' => 'Carregando...',
-        'processing'     => 'Processando...',
-        'search'         => 'Procure: ',
-        'zeroRecords'    => 'Nenhum registro encontrado',
-        'paginate'       => [
-            'first'    => 'Primeiro',
-            'last'     => 'Último',
-            'next'     => 'Próximo',
-            'previous' => 'Anterior',
-        ],
-        'aria' => [
-            'sortAscending'  => ': Ative para ordenar por ordem ascendente',
-            'sortDescending' => ': Ative para ordernar por ordem descendente',
-        ],
+    'emptyTable'     => 'Nenhum registro no momento',
+    'info'           => 'Mostrando _START_ de um total de _END_ de _TOTAL_ registros',
+    'infoEmpty'      => 'Mostrando 0 de 0 de um total de 0 registros',
+    'infoFiltered'   => '(filtrado De _MAX_ entradas totais)',
+    'infoPostFix'    => '',
+    'thousands'      => ',',
+    'lengthMenu'     => '_MENU_ itens por página',
+    'loadingRecords' => 'Carregando...',
+    'processing'     => 'Processando...',
+    'search'         => 'Procure: ',
+    'zeroRecords'    => 'Nenhum registro encontrado',
+    'paginate'       => [
+    'first'    => 'Primeiro',
+    'last'     => 'Último',
+    'next'     => 'Próximo',
+    'previous' => 'Anterior',
+    ],
+    'aria' => [
+    'sortAscending'  => ': Ative para ordenar por ordem ascendente',
+    'sortDescending' => ': Ative para ordernar por ordem descendente',
+    ],
 
     // global crud - errors
     'unauthorized_access' => 'Acesso não autorizado - você não tem as permissões necessárias para ver essa página.',
@@ -98,6 +99,10 @@ return [
     'reorder_success_message'      => 'Seu pedido foi salvo.',
     'reorder_error_title'          => 'Erro',
     'reorder_error_message'        => 'Seu item não foi salvo.',
+    
+       // CRUD yes/no
+    'yes' => 'Sim',
+    'no' => 'Não',
 
     // Fields
     'browse_uploads' => 'Procurar nos uploads',
@@ -113,4 +118,6 @@ return [
     'table_cant_add' => 'Não pode adicionar um novo :entity',
     'table_max_reached' => 'Número máximo de :max atingido',
 
-];
+    // File manager
+    'file_manager' => 'File Manager',
+    ];
